18ac90fdce20b814723c681ab2fcde6d14ae9b82
NTOable <pardsftw@gmail.com>
2025-10-07 04:07:57 +0800

    Commit and Push 4:07 A.M.

--- app/src/main/java/com/example/museum/MainActivity.kt
+++ app/src/main/java/com/example/museum/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.*
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.net.toUri // for parsing URLs to String >> "".toUri()
 
 
@@ -14,12 +15,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // store user input
+        // store button value
         val btn_1 = findViewById<ImageButton>(R.id.btn_1) // home
         val btn_2 = findViewById<ImageButton>(R.id.btn_2) // courses
         val btn_3 = findViewById<ImageButton>(R.id.btn_3) // blog
         val btn_4 = findViewById<ImageButton>(R.id.btn_4) // enroll
         val btn_5 = findViewById<ImageButton>(R.id.btn_5) // about
+        val sw = findViewById<SwitchCompat>(R.id.sw_minimal)
+        val tg = findViewById<ToggleButton>(R.id.tg_fun)
             // store url
             val h_pg = "https://www.ciit.edu.ph/".toUri()
             val c_pg = "https://www.ciit.edu.ph/programs-and-courses/".toUri()
@@ -52,5 +55,20 @@ class MainActivity : AppCompatActivity() {
 //                val un =
 //                    println("Welcome $un .")
 //            }
+            // switch button
+            val main_layout = findViewById<LinearLayout>(R.id.main)
+        sw?.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                main_layout.setBackgroundResource(R.drawable.minimal_mode)
+            } else {
+                main_layout.setBackgroundResource(R.drawable.bg_ciit)
+            }
+        }
+            // toggle button
+            tg?.setOnCheckedChangeListener {_, isChecked ->
+                if (isChecked) {
+                    Toast.makeText(this, "I'm just happy to be here!", Toast.LENGTH_SHORT) .show()
+                }
+            }
     }
 }
